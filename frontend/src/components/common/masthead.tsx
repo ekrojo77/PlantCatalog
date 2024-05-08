69c327d9c3e1844ea612ff7c85cf2510d9fcca76
ekrojo77 <even__kj@hotmail.com>
2024-05-08 18:18:57 +0200

    Improved design

--- frontend/src/components/common/masthead.tsx
+++ frontend/src/components/common/masthead.tsx
@@ -10,6 +10,11 @@ interface UserMenuProps {
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ onLogout }) => {
+  const positionerStyle: React.CSSProperties = {
+    minWidth: '100%',
+    zIndex: 1, 
+    left: '0',
+  };
   const menuStyle: React.CSSProperties = {
     backgroundColor: 'var(--primary-green)',
     border: '2px solid var(--dark-brown)',
@@ -19,18 +24,27 @@ const UserMenu: React.FC<UserMenuProps> = ({ onLogout }) => {
     right: '0',
     top: '100%',
     zIndex: 1,
+  };
+  const itemStyle: React.CSSProperties = {
+    padding: '10px',
+    cursor: 'pointer',
     color: 'var(--off-white)',
+  };
+  const separatorStyle : React.CSSProperties = {
+    height: '2px',
+    backgroundColor: 'var(--dark-brown)',
+    margin: '5px 0',
   };
   return (
     <Menu.Root>
       <Menu.Trigger aria-label="Open user menu">
         <Button>Menu</Button>
       </Menu.Trigger>
-      <Menu.Positioner>
+      <Menu.Positioner style={positionerStyle}>
         <Menu.Content style={menuStyle}>
-          <Menu.Item id="myInfo" aria-label="Go to My Info">My Info</Menu.Item>
-          <Menu.Separator />
-          <Menu.Item id="logout" aria-label="Log Out" onClick={onLogout}>Log Out</Menu.Item>
+          <Menu.Item id="myInfo" aria-label="Go to My Info" style={itemStyle}>My Info</Menu.Item>
+          <Menu.Separator style={separatorStyle}/>
+          <Menu.Item id="logout" aria-label="Log Out" onClick={onLogout} style={itemStyle}>Log Out</Menu.Item>
         </Menu.Content>
       </Menu.Positioner>
     </Menu.Root>
